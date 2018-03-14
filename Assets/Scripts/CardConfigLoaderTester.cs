530d2b90e9c3a6263da332cdac6cb9637b04328b
Kris Kalavantavanich <krismath.K@gmail.com>
2018-03-14 17:28:05 +0700

    Load card by json file

---
+++ Assets/Scripts/CardConfigLoaderTester.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CardConfigLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string cardConfigJsonPath = Application.dataPath + "/Config/cards_config.json";
+        if (File.Exists(cardConfigJsonPath))
+        {
+            string dataAsJson = File.ReadAllText(cardConfigJsonPath);
+            Debug.Log(CardConfigData.FromJSON(dataAsJson));
+        } else
+        {
+            Debug.LogError("CardConfigJson File does not exist! (value = " + cardConfigJsonPath + ")");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
