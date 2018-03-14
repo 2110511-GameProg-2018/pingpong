6cb9de3a4d233a2c70099c17ccce40c5d6a1945c
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2018-03-14 20:48:31 +0700

    Create Ball and Direction class

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private float speed;
+	private Direction location;
+
+	// Use this for initialization
+	void Start () {
+		speed = 0;
+		location = Direction.NONE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	int getLocation(){
+		return location;
+	}
+
+	void setLocation(Direction new){
+		location = new
+	}
+}
