b82c78006c81743b586868d8f5e8e5a76ae23315
Kris Kalavantavanich <krismath.K@gmail.com>
2018-03-14 17:35:26 +0700

    Make BaseCard comparable for sorting

--- Assets/Scripts/Cards/BaseCard.cs
+++ Assets/Scripts/Cards/BaseCard.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class BaseCard : MonoBehaviour {
+public class BaseCard : MonoBehaviour, System.IComparable<BaseCard>
+{
     string name;
     string id;
     Texture2D image;
     string description;
     float ballSpeed;
     
+    public int CompareTo(BaseCard other)
+    {
+        if (other == null) return 1;
+        return id.CompareTo(other.id);
+    }
     
 }
