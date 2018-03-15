086f05cd5b964143b1ea3ffa8220f675ac2b75ad
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2018-03-15 13:45:33 +0700

    Add text to show ball speed

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,28 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour {
 
 	private float speed;
 	private Direction location;
+	public Text speedText;
 
 	// Use this for initialization
 	void Start () {
 		speed = 0;
 		location = Direction.NONE;
+		SetSpeedText();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		SetSpeedText();
+		if (Input.GetKeyDown ("space")) {
+			speed += 1;
+		}
 	}
 
-	int getLocation(){
+	float getSpeed(){
+		return speed;
+	}
+
+	void setSpeed(float new_speed){
+		speed = new_speed;
+	}
+
+	Direction getLocation(){
 		return location;
 	}
 
-	void setLocation(Direction new){
-		location = new
+	void setLocation(Direction new_direction){
+		location = new_direction;
 	}
+
+	void SetSpeedText()
+    {
+        speedText.text = "Ball speed: " + speed.ToString ();
+
+    }
 }
