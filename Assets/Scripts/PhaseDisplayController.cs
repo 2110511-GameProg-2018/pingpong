56fe4eea24186a3dd98c7b67cd0d1f3754adfb3d
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2018-03-17 00:48:00 +0700

    add logic to select phase and change size of grid

---
+++ Assets/Scripts/PhaseDisplayController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhaseDisplayController : MonoBehaviour {
+	public Phase selectedPhase;
+	public PhaseComponent Stand_by;
+	public PhaseComponent Draw;
+	public PhaseComponent Defend;
+	public PhaseComponent Guess;
+	public PhaseComponent Counter;
+	public PhaseComponent Attack;
+	public PhaseComponent End;
+	RectTransform parent;
+	GridLayoutGroup grid;
+
+	// Use this for initialization
+	void Start () {
+		selectedPhase = Phase.STAND_BY;
+		parent = GetComponent<RectTransform> ();
+		grid = GetComponent<GridLayoutGroup> ();
+		DynamicGrid ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DynamicGrid ();
+		ClearState ();
+		switch (selectedPhase) {
+			case Phase.STAND_BY:
+				Stand_by.setState (true);
+				break;
+			case Phase.DRAW:
+				Draw.setState (true);
+				break;
+			case Phase.DEFEND:
+				Defend.setState (true);
+				break;
+			case Phase.GUESS:
+				Guess.setState (true);
+				break;
+			case Phase.COUNTER:
+				Counter.setState (true);
+				break;
+			case Phase.ATTACK:
+				Attack.setState (true);
+				break;
+			case Phase.END:
+				End.setState (true);
+				break;
+		}
+	}
+
+	void ClearState(){
+		Stand_by.setState (false);
+		Draw.setState (false);
+		Defend.setState (false);
+		Guess.setState (false);
+		Counter.setState (false);
+		Attack.setState (false);
+		End.setState (false);
+	}
+
+	void DynamicGrid(){
+		grid.cellSize = new Vector2 (2 * parent.rect.width / 3, parent.rect.height / 8);
+	}
+
+	public void setSelectedPhase(Phase newPhase){
+		selectedPhase = newPhase;
+	}
+}
