c09045c1e4eabeb2148caa96a9caba5b8ed428aa
Krittakan <ragunakan@gmail.com>
2018-03-16 16:55:41 +0700

    IEffect + EffectCollector

---
+++ Assets/Scripts/IEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IEffect
+{
+    public int effectType;
+    //1 change speed
+    //2 other
+    public int effectParameter;
+    public int turn;
+
+    public IEffect(int effectType, int effectParameter,int turn)
+    {
+        this.effectType = effectType;
+        this.effectParameter = effectParameter;
+        this.turn = turn;
+    }
+}
