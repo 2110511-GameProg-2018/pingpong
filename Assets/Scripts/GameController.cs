0004aaa2585e4c48306aa1f7a9ff169dd9fd3150
munggumy <iknobkb@gmail.com>
2018-03-18 22:17:40 +0700

    implement Player and Field to gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+
+	public Player player;
+	public Field field;
 	
     InnerPhase currentPhase;
-    PhaseModel pm = GameObject.FindObjectOfType<PhaseModel>();
+    PhaseModel pm;
     
     // Use this for initialization
     void Start () {
+		pm = GameObject.FindObjectOfType<PhaseModel> ();
         currentPhase = InnerPhase.INITIATE_GAME;
     }
 	
@@ -37,40 +41,66 @@ public class GameController : MonoBehaviour {
                 break;
             case InnerPhase.DRAW:
                 draw();
-                if (false /* cannot draw a card, deck is empty */)
+				if (!canDrawCard)
                 {
                     nextPhase = InnerPhase.LOSE;
                 } else
                 {
-                    nextPhase = InnerPhase.DEFEND_STANDBY;
+                    nextPhase = InnerPhase.DRAWING;
                 }
                 break;
+			case InnerPhase.DRAWING:
+				drawing ();
+				if (!player.IsDrawFinished())
+				{
+					nextPhase = InnerPhase.DRAWING;
+				} else
+				{
+					nextPhase = InnerPhase.DEFEND_STANDBY;
+				}
+				break;
             case InnerPhase.DEFEND_STANDBY:
                 defendStandby();
                 nextPhase = InnerPhase.DEFEND_CARD_SELECT;
                 break;
             case InnerPhase.DEFEND_CARD_SELECT:
                 defendCardSelect();
-                if (null != null /* A card is selected */)
+				if (selectedCard != null)
                 {
-                    nextPhase = InnerPhase.DEFEND_CARD_EXECUTION;
-                } else if (true /* skip button is pressed */)
+					nextPhase = InnerPhase.DEFEND_CARD_SELECTING;
+				} else if (Input.GetKeyDown(KeyCode.D) /* skip button is pressed */)
                 {
-                    nextPhase = InnerPhase.DEFEND_FIELD_DIRECTION;
+					player.SetHandSelectableType (false, false, false, false);
+					nextPhase = InnerPhase.SET_DEFEND_FIELD;
                 } else
                 {
                     nextPhase = InnerPhase.DEFEND_CARD_SELECT;
                 }
                 break;
+			case InnerPhase.DEFEND_CARD_SELECTING:
+				defendCardSelecting();
+				if (player.IsSelectFinished())
+				{
+					player.SetHandSelectableType (false, false, false, false);
+					nextPhase = InnerPhase.DEFEND_CARD_EXECUTION;
+				} else
+				{
+					nextPhase = InnerPhase.DEFEND_CARD_SELECTING;
+				}
+				break;
             case InnerPhase.DEFEND_CARD_EXECUTION:
                 defendCardExecution();
-                nextPhase = InnerPhase.DEFEND_FIELD_DIRECTION;
+				nextPhase = InnerPhase.SET_DEFEND_FIELD;
                 break;
+			case InnerPhase.SET_DEFEND_FIELD:
+				setDefendField ();
+				nextPhase = InnerPhase.DEFEND_FIELD_DIRECTION;
+				break;
             case InnerPhase.DEFEND_FIELD_DIRECTION:
                 defendFieldDirection();
-                if (null != null /* Direction is selected */ )
+				if (defendDirection != Direction.NONE /* Direction is selected */ )
                 {
-                    if (true /* guessed right */)
+					if (defendDirection == attackDirection || attackDirection == Direction.NONE /* guessed right */)
                     {
                         nextPhase = InnerPhase.CONDITION_STANDBY;
                     }
@@ -82,6 +112,9 @@ public class GameController : MonoBehaviour {
                     {
                         nextPhase = InnerPhase.LOSE;
                     }
+					attackDirection = Direction.NONE;
+					defendDirection = Direction.NONE;
+					field.gameObject.SetActive(false);
                 } else /* Direction is not selected */
                 {  
                     nextPhase = InnerPhase.DEFEND_FIELD_DIRECTION;
@@ -93,15 +126,15 @@ public class GameController : MonoBehaviour {
                 break;
             case InnerPhase.CONDITION_CARD_SELECT:
                 conditionCardSelect();
-                if (null != null /* Card is selected */)
+				if (selectedCard != null /* Card is selected */)
                 {
-                    if (true /* card is condition card */)
+					if (selectedCard.GetBaseCard().GetCardType() == CardType.Effect /* card is condition card */)
                     {
-                        nextPhase = InnerPhase.CONDITION_CARD_EXECUTION;
+                        nextPhase = InnerPhase.CONDITION_CARD_SELECTING;
                     }
-                    else if (true /* card is attack card */ )
+					else if (selectedCard.GetBaseCard().GetCardType() == CardType.Attack /* card is attack card */ )
                     {
-                        nextPhase = InnerPhase.ATTACK_CARD_EXECUTION;
+						nextPhase = InnerPhase.ATTACK_CARD_SELECTING;
                     }
                     else
                     {
@@ -109,8 +142,9 @@ public class GameController : MonoBehaviour {
                         nextPhase = InnerPhase.ERROR;
                     }
                 }
-                else if (true /* Simple Attack button pressed */)
+				else if (Input.GetKeyDown(KeyCode.A) /* Simple Attack button pressed */)
                 {
+					player.SetHandSelectableType (false, false, false, false);
                     nextPhase = InnerPhase.SIMPLE_ATTACK;
                 }
                 else
@@ -118,6 +152,17 @@ public class GameController : MonoBehaviour {
                     nextPhase = InnerPhase.CONDITION_CARD_SELECT;
                 }
                 break;
+			case InnerPhase.CONDITION_CARD_SELECTING:
+				conditionCardSelecting();
+				if (player.IsSelectFinished())
+				{
+					player.SetHandSelectableType (false, false, false, false);
+					nextPhase = InnerPhase.CONDITION_CARD_EXECUTION;
+				} else
+				{
+					nextPhase = InnerPhase.CONDITION_CARD_SELECTING;
+				}
+				break;
             case InnerPhase.CONDITION_CARD_EXECUTION:
                 conditionCardExecution();
                 nextPhase = InnerPhase.ATTACK_STANDBY;
@@ -128,35 +173,58 @@ public class GameController : MonoBehaviour {
                 break;
             case InnerPhase.ATTACK_CARD_SELECT:
                 attackCardSelect();
-                if (null != null /* a card is selected -- not null */)
+				if (selectedCard != null /* a card is selected -- not null */)
                 {
-                    if (true /* card is of attack type */)
+					if (selectedCard.GetBaseCard().GetCardType() == CardType.Attack /* card is of attack type */)
                     {
-                        nextPhase = InnerPhase.ATTACK_CARD_EXECUTION;
+						nextPhase = InnerPhase.ATTACK_CARD_SELECTING;
                     } else
                     {
                         Debug.LogError("Invalid card selected (not attack type)");
                         nextPhase = InnerPhase.ERROR;
                     }
-                } else if (true /* simple attack button is pressed */ )
+				} else if (Input.GetKeyDown(KeyCode.A) /* simple attack button is pressed */ )
                 {
+					player.SetHandSelectableType (false, false, false, false);
                     nextPhase = InnerPhase.SIMPLE_ATTACK;
                 } else
                 {
                     nextPhase = InnerPhase.ATTACK_CARD_SELECT;
                 }
                 break;
+			case InnerPhase.ATTACK_CARD_SELECTING:
+				attackCardSelecting();
+				if (player.IsSelectFinished())
+				{
+					player.SetHandSelectableType (false, false, false, false);
+					nextPhase = InnerPhase.ATTACK_CARD_EXECUTION;
+				} else
+				{
+					nextPhase = InnerPhase.ATTACK_CARD_SELECTING;
+				}
+				break;
             case InnerPhase.ATTACK_CARD_EXECUTION:
                 attackCardExecution();
-                nextPhase = InnerPhase.ATTACK_FIELD_DIRECTION;
+				nextPhase = InnerPhase.SET_ATTACK_FIELD;
                 break;
-            case InnerPhase.ATTACK_FIELD_DIRECTION:
-                attackFieldDirection();
-                nextPhase = InnerPhase.END;
+			case InnerPhase.SET_ATTACK_FIELD:
+				setAttackField ();
+				nextPhase = InnerPhase.ATTACK_FIELD_DIRECTION;
+				break;
+			case InnerPhase.ATTACK_FIELD_DIRECTION:
+				attackFieldDirection ();
+				if (attackDirection != Direction.NONE /* Direction is selected */ )
+				{
+					nextPhase = InnerPhase.END;
+					field.gameObject.SetActive(false);
+				} else /* Direction is not selected */
+				{  
+					nextPhase = InnerPhase.ATTACK_FIELD_DIRECTION;
+				}
                 break;
             case InnerPhase.SIMPLE_ATTACK:
                 simpleAttack();
-                nextPhase = InnerPhase.ATTACK_FIELD_DIRECTION;
+				nextPhase = InnerPhase.SET_ATTACK_FIELD;
                 break;
             case InnerPhase.LOSE:
                 lose();
@@ -179,64 +247,95 @@ public class GameController : MonoBehaviour {
 
     private void initializeGame()
     {
-
+		player.Initialize ();
+		field.gameObject.SetActive (false);
     }
     private void standby()
     {
 
     }
 
+	bool canDrawCard = true;
     private void draw()
     {
-
+		canDrawCard = player.Draw ();
     }
+
+	private void drawing()
+	{
+		
+	}
 
     private void defendStandby()
     {
-
+		player.SetHandSelectableType (false, true, false, false);
     }
-
+	BaseCardComponent selectedCard;
     private void defendCardSelect()
     {
-
+		selectedCard = player.SelectCard ();
     }
-
+	private void defendCardSelecting()
+	{
+		
+	}
     private void defendCardExecution()
     {
-
+		selectedCard = null;
     }
+	private void setDefendField()
+	{
+		field.setDirectionState (true, true, true, true);
+		field.gameObject.SetActive (true);
+	}
+	Direction defendDirection = Direction.NONE;
     private void defendFieldDirection()
     {
-
+		defendDirection = field.getInputDirection ();
     }
+
     private void conditionStandby()
     {
-
+		player.SetHandSelectableType (true, false, true, false);
     }
 
     private void conditionCardSelect()
     {
-
+		selectedCard = player.SelectCard ();
     }
+	private void conditionCardSelecting()
+	{
+
+	}
     private void conditionCardExecution()
     {
-
+		selectedCard = null;
     }
     private void attackStandby()
     {
-
+		player.SetHandSelectableType (true, false, false, false);
     }
     private void attackCardSelect()
     {
-
+		selectedCard = player.SelectCard ();
     }
+	private void attackCardSelecting()
+	{
+		
+	}
     private void attackCardExecution()
     {
-
+		selectedCard = null;
     }
+	private void setAttackField()
+	{
+		field.setDirectionState (true, true, true, true);
+		field.gameObject.SetActive (true);
+	}
+	Direction attackDirection = Direction.NONE;
     private void attackFieldDirection()
     {
-
+		attackDirection = field.getInputDirection ();
     }
     private void simpleAttack()
     {
