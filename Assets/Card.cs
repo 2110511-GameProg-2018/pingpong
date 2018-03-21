f1d7c5da702779ac2d87cc9703e5e40d4c670b42
newplyz <art_gif_52@hotmail.com>
2018-03-21 16:44:04 +0700

    card

---
+++ Assets/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Card : ScriptableObject {
+
+    public string nameTH;
+    public string type;
+    public int cid;
+    public string descriptionTh;
+    public Sprite Image;
+}
---
+++ Assets/CardDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+
+    public Card card;
+    public Text nameText;
+    public Text typeText;
+    public Text cidText;
+    public Text descriptionText;
+    public Image Image;
+
+    // Use this for initialization
+    void Start()
+    {
+        nameText.text = card.nameTH;
+        typeText.text = card.type;
+        cidText.text = card.cid.ToString();
+        descriptionText.text = card.descriptionTh;
+        Image.sprite = card.Image;
+    }
+
+}
